e454de819d347d1000b5b305d1a950d45e0ee44e
SHRIVATSA.MISHRA <shrivatsa.m@mygetepay.com>
2025-08-04 19:27:40 +0530

    created new entry creator

--- speak.py
+++ speak.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-users = ["Harsh", "Deepak", "Anjali"]
+users = ["Harsh", "Deepak", "Anjali","Lokesh"]
 name = input("Enter your name: ")
 if name in users:
     engine.say(f"Welcome {name}, you are logged in.")
